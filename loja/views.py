1a4a8aafefd667f8c70fe4bb8452e47ee671d659
Leonardo Breda <leonardo-breda1@hotmail.com>
2024-09-19 17:30:21 -0300

    Pagina de checkout e condicao usuario autenticado no html

--- loja/views.py
+++ loja/views.py
@@ -153,7 +153,29 @@ def carrinho(request):
 
 
 def checkout(request):
-    return render(request, 'checkout.html')
+    if request.user.is_authenticated:
+        cliente = request.user.cliente
+    else:
+        if request.COOKIES.get('id_sessao'):
+            id_sessao = request.COOKIES.get('id_sessao')
+            cliente, criado = Cliente.objects.get_or_create(id_sessao=id_sessao)
+        else:
+            return redirect('loja')
+
+    pedido, criado = Pedido.objects.get_or_create(cliente=cliente, finalizado=False)
+    enderecos = Endereco.objects.filter(cliente=cliente)
+
+    context = {
+        'pedido': pedido,
+        'enderecos': enderecos,
+    }
+
+    return render(request, 'checkout.html', context)
+
+
+def adicionar_endereco(request):
+    context = {}
+    return render(request, 'adicionar_endereco.html', context)
 
 
 def minha_conta(request):
